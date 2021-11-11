5f0ffb245f736cee9975799a2f9ecc4bfe7e00d8
Nicole Wert <nicalexxandra@gmail.com>
2021-11-11 12:05:20 +0900

    #2 build: create base project with create-react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
+import './App.css';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Add routes here */}
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
