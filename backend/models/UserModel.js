757a09b12895032f9c798e3a43d48da430f046ba
Nicole Wert <nicalexxandra@gmail.com>
2021-11-26 23:38:03 +0900

    #9 feat: create user schema & model

---
+++ backend/models/UserModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String, 
+        required: true
+    }, 
+    email: {
+        type: String, 
+        required: true
+    }, 
+    password: {
+        type: String, 
+        required: true
+    }, 
+    rpassword : {
+        type: String, 
+        required: true
+    }, 
+    date: {
+        type: Date, 
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
