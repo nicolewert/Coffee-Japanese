2aabe372f92d59689a1a1268694046ebb7500f52
Nicole Wert <nicalexxandra@gmail.com>
2021-11-27 22:40:13 +0900

    #15: update register post route to include validation, password hashing, and access token generation

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,21 +1,49 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/UserModel')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const bcrypt = require('bcryptjs')
 
-router.post('/Register', (req, res) => {
-    const user = new User({
-        username: req.body.username, 
-        email: req.body.email,
-        password: req.body.password,
-        rpassword: req.body.rpassword
-    })
-    user.save()
-    .then(data =>{
-        res.json(data)
-    })
-    .catch(error =>{
-        res.json(error)
-    })
+router.post('/register', async(req, res) => { 
+    try{
+        const {username, email, password, rpassword} = req.body
+        
+        //Validate
+        if(!(username && email && password && rpassword)){
+            return res.status(400).send("All fields are required")
+        }
+
+        if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+            return res.status(400).send("Invalid Email.")
+        }
+
+        if(password !== rpassword){
+            return res.status(400).send("Passwords must match.")
+        }
+
+        const existingUser = await User.findOne({email})
+        if (existingUser){
+            return res.status(409).send("User already exists. Please Login.")
+        }
+
+        //Salt & Hash password 
+        const hashedPassword = await bcrypt.hash(password, 10)
+        
+        //Create access token
+        const token = jwt.sign({ username: username }, process.env.TOKEN_SECRET,{expiresIn: "3h"})
+        
+        //Create user in db
+        const user = await User.create({
+            username: username,
+            email: email.toLowerCase(),
+            password: hashedPassword,
+            token: token
+        })
+        res.status(201).json({token: user.token})
+    } catch(err) {
+        res.status(500).send(err)
+    }
 })
 
 module.exports = router
