8c96005062d00a16ed04e5bf4db63d78af9e322d
Nicole Wert <nicalexxandra@gmail.com>
2021-12-07 12:05:02 +0900

    #13 refactor: remove state from Login Page (State should be controlled from LoginForm)

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,16 +1,10 @@
-import React, {useState} from 'react'; 
+import React from 'react'; 
 import LoginForm from "../../components/LoginForm/LoginForm";
 import classes from './Login.module.css'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Login = () =>{
-    const [user, setUser] = useState({email:"", password:""}); 
-    const [error, setError] = useState(""); 
-
-    const Login = info =>{
-        console.log(info)
-    }
 
     return(
         <div className={classes.container}>
