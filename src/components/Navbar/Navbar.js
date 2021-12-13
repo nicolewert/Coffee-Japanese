1b35625b57968a7f604ffc3eceb14b8f9d8387ff
Nicole Wert <nicalexxandra@gmail.com>
2021-12-13 20:59:00 +0900

    #27 feat: create navbar component & style

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import classes from './Navbar.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () =>{
+    const [sideNav, setSideNav] = useState(false)
+    const toggleSideNav = () => setSideNav(!sideNav)
+
+    return(
+        <>
+        <nav className={classes.navbar}>
+            <FontAwesomeIcon icon={faCoffee} className={classes.icon}/>
+            <h1 className={classes.navItem}>Coffee Japanese</h1>
+            <FontAwesomeIcon icon={faBars} className={classes.icon} onClick={toggleSideNav}/>
+        </nav>
+        <div className={sideNav? classes.sideNavActive : classes.sideNav}>
+            <FontAwesomeIcon icon={faTimes} className={classes.icon +" "+ classes.sideNavClose} onClick={toggleSideNav}/>
+            <ul>
+                <li className={classes.sideNavItem}>
+                    <a href="/">User Profile</a>
+                </li>
+                <li className={classes.sideNavItem}>
+                    <a href="/">Contact Us</a>
+                </li>
+                <li className={classes.sideNavItem}>
+                    <a href="/login">Logout</a>
+                </li>
+            </ul>
+        </div>
+        </>
+    ) 
+}
+
+export default Navbar
