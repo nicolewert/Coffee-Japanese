b6cecfeddd79e24e5da14ae8d8c939f8d131df8e
Nicole Wert <nicalexxandra@gmail.com>
2022-01-25 23:02:52 -0500

    #31 refactor: remove old routes import in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const routes = require('./routes/routes')
 const cors = require('cors')
 
 const app = express()
