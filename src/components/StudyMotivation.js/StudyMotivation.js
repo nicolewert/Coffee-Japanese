0388a84e0cd18f6c97398e97543f15e486fd8294
Nicole Wert <nicalexxandra@gmail.com>
2022-02-13 16:26:47 -0500

    #33 feat: render and style homeData on the Home page

--- src/components/StudyMotivation.js/StudyMotivation.js
+++ src/components/StudyMotivation.js/StudyMotivation.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import classes from './StudyMotivation.module.css'
 
-const StudyMotivation = () => {
+const StudyMotivation = ({quote}) => {
     return(
         <div className={classes.container}>
             <h3>Study Motivation</h3>
             <div className={classes.quoteBox}>
-                <p className={classes.quote}>quote</p>
+                <p className={classes.quote}>{quote && quote.quote}</p>
             </div>
         </div>
     )
