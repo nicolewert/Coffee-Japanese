acd056fe1788c349c4f33fb5fa69a3b0fb7ab0bc
Nicole Wert <nicalexxandra@gmail.com>
2022-02-17 21:51:51 -0500

    #34 feat: add coffee-lesson backend route

--- backend/routes/api/lessons.js
+++ backend/routes/api/lessons.js
@@ -1,5 +1,15 @@
 const express = require('express'); 
 const router = express.Router()
+const lessons = require('../../models/LessonModel')
 
+// @route   GET route
+// @desc    Return a coffee lesson from db
+// @access  Private
+router.get("/", async(req, res)=>{
+    const count = await lessons.count().exec()
+    var random = Math.floor(Math.random()*count)
+    const result = await lessons.findOne().skip(random).exec()
+    res.json(result)
+})
 
 module.exports = router; 
