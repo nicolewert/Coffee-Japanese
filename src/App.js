2377eceb2bb6d90c4ea9e248a857c241b614d527
Nicole Wert <nicalexxandra@gmail.com>
2022-02-18 22:38:34 -0500

    #41 refactor: make app open to login page

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Router>
         <Routes>
         
-            <Route path="/login" element={<Login/>}/>
+            <Route path="/" element={<Login/>}/>
 
             <Route path="/register" element={<Register/>}/>
 
