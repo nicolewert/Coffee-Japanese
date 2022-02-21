dc911750f78b00fc2f909701952354b5d11301ce
Nicole Wert <nicalexxandra@gmail.com>
2022-02-20 20:53:01 -0500

    #41 feat: persist store with redux-persist

--- src/App.js
+++ src/App.js
@@ -10,32 +10,36 @@ import PageNotFound from './pages/PageNotFound/PageNotFound'
 
 //Redux 
 import {Provider} from 'react-redux'; 
-import store from './store'; 
+import {store, persistor} from './store'; 
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <Routes>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Routes>
         
-            <Route path="/" element={<Login/>}/>
+              <Route path="/" element={<Login/>}/>
 
-            <Route path="/register" element={<Register/>}/>
+              <Route path="/register" element={<Register/>}/>
 
-            <Route 
-              path="/home" 
-              element={<PrivateRoute component ={Home}/>}
-            />
+              <Route 
+                path="/home" 
+                element={<PrivateRoute component ={Home}/>}
+              />
 
-            <Route 
-              path="/coffee-lesson" 
-              element={<PrivateRoute component={CoffeeLesson}/>}
-            />
+              <Route 
+                path="/coffee-lesson" 
+                element={<PrivateRoute component={CoffeeLesson}/>}
+              />
 
-            <Route path="*" element={<PageNotFound/>}></Route>
+              <Route path="*" element={<PageNotFound/>}></Route>
 
-        </Routes>
-      </Router>
+          </Routes>
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,12 +1,35 @@
-import {createStore, applyMiddleware} from 'redux'; 
-import {composeWithDevTools} from 'redux-devtools-extension'; 
-import thunk from 'redux-thunk'
+import {configureStore} from '@reduxjs/toolkit'
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import {persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import rootReducer from './reducers'; 
 
-const initialState = {}; 
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+  
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+  
+  const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => // adds redux-thunk middleware by default 
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+  })
 
-const middleware = [thunk]; 
-
-const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware))); 
-
-export default store; 
+  const persistor = persistStore(store)
+  
+export {store, persistor}; 
