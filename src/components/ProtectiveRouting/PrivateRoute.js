633817cdc64c949966333cbeffbbf22ab10d2275
Nicole Wert <nicalexxandra@gmail.com>
2022-02-20 20:56:14 -0500

    #41 refactor: update login route to navigate to

--- src/components/ProtectiveRouting/PrivateRoute.js
+++ src/components/ProtectiveRouting/PrivateRoute.js
@@ -6,7 +6,7 @@ const PrivateRoute = ({component: Component}) =>{
     const isAuth = useSelector(state=> state.auth.isAuthenticated)
     
     if (isAuth) return <Component/>
-    return <Navigate to="/login"/>
+    return <Navigate to="/"/>
 }
 
 export default PrivateRoute
