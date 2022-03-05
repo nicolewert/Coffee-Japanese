cc7cd4ec3db08a5889c9a3662a83da0272e863d9
Nicole Wert <nicalexxandra@gmail.com>
2022-03-04 21:19:28 -0500

    #52 feat: create redux action and reducer for persisted user state

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,5 @@ export const authFailure = 'auth/authFailure'
 export const loginSuccess = 'auth/loginUser'
 export const logout = 'auth/logoutUser'
 
+export const getUser = 'users/getUser'
+export const userError = 'users/userError'
