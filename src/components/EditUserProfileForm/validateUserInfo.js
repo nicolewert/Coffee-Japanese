3d5d4b213c5b8b7d05755d0242b5075906d9bf09
Nicole Wert <nicalexxandra@gmail.com>
2022-03-12 14:30:18 -0500

    #53 refactor: only validate email if given, remove validation requiring fields

--- src/components/EditUserProfileForm/validateUserInfo.js
+++ src/components/EditUserProfileForm/validateUserInfo.js
@@ -1,15 +1,7 @@
 export default function validateEditUserProfile(userInfo){
     let errors = {}
-
-    //username
-    if(!userInfo.username.trim()){
-        errors.username = "Username required"
-    }
-
     //email
-    if(!userInfo.email){
-        errors.email = "Email required"
-    } else if(!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(userInfo.email)){
+    if(userInfo.email && !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(userInfo.email)){
         errors.email ="Email address is invalid"
     }
 
